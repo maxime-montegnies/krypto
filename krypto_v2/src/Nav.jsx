d0fbb3dd3f3dad0488e466b0c94c2d6933234817
Maxime Montegnies <mmontegnies@apple.com>
2023-06-07 23:21:40 -0700

    wip

--- krypto_v2/src/Nav.jsx
+++ krypto_v2/src/Nav.jsx
@@ -1,34 +1,44 @@
-import styles from './style/style.module.scss';
+import styles from "./style/style.module.scss";
 import { NavLink } from "react-router-dom";
 
-import { useRef, useEffect, useState } from 'react'
-import useApp from './store/useApp';
+import { useRef, useEffect, useState } from "react";
+import useApp from "./store/useApp";
 
-export default function Nav({ }) {
-    const nav = [
-        {
-            label:"Home",
-            path:"/"
-        },
-        {
-            label:"Blogs",
-            path:"/blogs"
-        },
-        {
-            label:"Contact",
-            path:"/contact"
-        }
-    ]
-    return <div className={styles.navigation}>
-        <div className={styles.navigation_logo}></div>
-        <nav>
-            <ul className={styles.navigation_items}>
-                {nav.map(function(element, i) {
-                    return <li>
-                        <NavLink key={i} className={({ isActive }) => (isActive ? styles.active : '')} to={element.path} >{element.label}</NavLink>
-                    </li>
-                })}                
-            </ul>
-        </nav>
+export default function Nav({}) {
+  const nav = [
+    {
+      label: "Home",
+      path: "/",
+    },
+    {
+      label: "Blogs",
+      path: "/blogs",
+    },
+    {
+      label: "Contact",
+      path: "/contact",
+    },
+  ];
+  return (
+    <div className={styles.navigation}>
+      <div className={styles.navigation_logo}></div>
+      <nav>
+        <ul className={styles.navigation_items}>
+          {nav.map(function (element, i) {
+            return (
+              <li>
+                <NavLink
+                  key={i}
+                  className={({ isActive }) => (isActive ? styles.active : "")}
+                  to={element.path}
+                >
+                  {element.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </div>
+  );
 }
--- krypto_v2/src/pages/Home.jsx
+++ krypto_v2/src/pages/Home.jsx
@@ -1,3 +1,57 @@
-export default function Home({ }) {
-    return <h1>🏠 Home</h1>;
-  };
+import styles from "../style/style.module.scss";
+import { NavLink } from "react-router-dom";
+
+export default function Home({}) {
+  const pools = [
+    {
+        label:"Le Corbusier",
+        img:"/images/discover__pool_1.jfif"
+    },
+    {
+        label:"Eiffel",
+        img:"/images/discover__pool_2.jfif"
+    },
+    {
+        label:"Lloyd Wright",
+        img:"/images/discover__pool_3.jfif"
+    },
+]
+  return (
+    <div>
+      <div className={styles.home_hero__section}>
+        <div className={styles.home_hero__section_text}>
+          <h2>Investissez dans l’immobilier durable en toute simplicité.</h2>
+          <p>
+            Notre mission est de rendre l'investissement immobilier durable,
+            rentable, vertueux et accessible à tous, par le biais d'un Security
+            Token.
+          </p>
+        </div>
+        <ul className={styles.home_hero__section_nav}>
+          <li>
+            <NavLink to="/blogs">Commencez à investir</NavLink>
+          </li>
+          <li>
+            <NavLink to="/yolo">En apprendre plus</NavLink>
+          </li>
+        </ul>
+        <figure></figure>
+      </div>
+
+      <div className={styles.home_discover__pool}>
+        <div className={styles.home_discover__pool__title}>
+          <h3>Investissement</h3>
+          <h2>Découvrez nos pools d’investissement</h2>
+        </div>
+        <ul className={styles.home_discover__pool__content}>
+        {pools.map(function (element, i) {
+            return (
+              <li style={{backgroundImage:"url('"+element.img+"')"}}>{element.label}</li>
+            );
+          })}
+          <li>swag</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
